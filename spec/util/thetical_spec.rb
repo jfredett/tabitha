03a7ced8ad17db028c2b9ad9682db9eb54c895a4
Joe Fredette <jfredett@gmail.com>
2025-02-01 16:30:18 -0500

    remove debug lines

--- spec/util/thetical_spec.rb
+++ spec/util/thetical_spec.rb
@@ -4,17 +4,14 @@ RSpec.describe Tabitha::Util::Thetical do
   before(:all) do
     SetOfNonemptyStrings = Tabitha::Util::Thetical.define do |th|
       th.accept!(name: "Strings only") do |item|
-        STDERR.puts "in global accept constraint"
         item.is_a?(String)
       end
 
       th.reject!(methods: [:<<], name: "nonzero-length") do |item|
-        STDERR.puts "in << reject constraint"
         item.respond_to?(:length) && item.length == 0
       end
 
       th.accept!(name: "Unique lengths required") do |left, right|
-        STDERR.puts "in global accept 2-arity constraint"
         left.length != right.length
       end.describe("Additional Description Possible")
     end
